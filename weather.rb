8965f80022e9e76a9fd50bbcb315fb1009b68533
Elena Jung <mail@elenajung.de>
2015-03-03 00:17:01 +0100

    Mulitple Cities implemented

--- weather.rb
+++ weather.rb
@@ -2,8 +2,13 @@ require 'open-uri'
 require 'json'
 
 def ask_location
-	print 'Where would you like to travel today?'
-	gets.chomp.capitalize
+	print "Where would you like to travel today?\n"
+	# answer = gets.chomp.split(',')
+	answer = "paris, berlin".split(',')
+	answer.map! do |city|
+		city.strip.capitalize
+	end
+	answer
 end
 
 def weather_data(city)
@@ -11,13 +16,35 @@ def weather_data(city)
 	contents = file.read
 	parsed = JSON.parse(contents)
 	weather = parsed["weather"]
-	weather[0].fetch('description')
+	main = parsed["main"]
+	data = Hash.new
+	data[:city] = city
+	data[:description] = weather[0].fetch('description')
+	# Temperature in Kelvin (subtract 273.15 to convert to Celsius)
+	data[:temperature] = ((main["temp"] - 273.15).round(1)).to_s + "°C"
+	data[:humidity] = main["humidity"]
+	data
 end
 
-def weather_output(x,y)
-	"The weather in #{ x } is : #{y}"
+def format_table(results)
+	# find way to print results as table
+	puts results
 end
 
 location = ask_location
-city_weather_description = weather_data(location)
-puts weather_output(location, city_weather_description)
+results = Array.new
+
+table_header = Hash.new
+table_header[:city] = "Stadt"
+table_header[:description] = "Wetter"
+table_header[:temperature] = "Temperatur"
+table_header[:humidity] = "Humidity"
+
+results << table_header
+
+location.each do |one_location|
+	data = weather_data(one_location)
+	results << data
+end
+
+puts format_table(results)
