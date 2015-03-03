37aaf7830b20fb3cc2dc26719b0bdb2113cad7ba
Elena Jung <mail@elenajung.de>
2015-03-03 20:47:38 +0100

    homework feedback, adding table format funktion and further to dos

--- weather.rb
+++ weather.rb
@@ -3,8 +3,8 @@ require 'json'
 
 def ask_location
 	print "Where would you like to travel today?\n"
-	# answer = gets.chomp.split(',')
-	answer = "paris, berlin".split(',')
+	answer = gets.chomp.split(',')
+	# answer = "paris, berlin, london".split(',')
 	answer.map! do |city|
 		city.strip.capitalize
 	end
@@ -22,21 +22,34 @@ def weather_data(city)
 	data[:description] = weather[0].fetch('description')
 	# Temperature in Kelvin (subtract 273.15 to convert to Celsius)
 	data[:temperature] = ((main["temp"] - 273.15).round(1)).to_s + "°C"
-	data[:humidity] = main["humidity"]
+	data[:humidity] = main["humidity"].to_s + "%"
 	data
 end
 
-def format_table(results)
+def format_table(rows)
 	# find way to print results as table
-	puts results
+	# puts results
+	
+	rows.each do |row|
+		print_single_row(row)
+		puts row.class
+		puts "--------------------------------------------------------------------"
+	end
+end
+	
+def print_single_row(row)
+	print row[:city].ljust(15)
+	print row[:description].ljust(15)
+	print row[:temperature].to_s.rjust(15)
+	print row[:humidity].to_s.rjust(15) + "\n"
 end
 
 location = ask_location
 results = Array.new
 
 table_header = Hash.new
-table_header[:city] = "Stadt"
-table_header[:description] = "Wetter"
+table_header[:city] = "City"
+table_header[:description] = "Weather"
 table_header[:temperature] = "Temperatur"
 table_header[:humidity] = "Humidity"
 
@@ -47,4 +60,8 @@ location.each do |one_location|
 	results << data
 end
 
-puts format_table(results)
+format_table(results)
+
+# to do
+# splitting weather_data into separated functions
+# save api uri in one global variable
